a0d4b33d75a66fcee218ce10f03a6c9380d99569
David <nacevskidavid@gmail.com>
2021-04-11 23:58:03 -0500

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <new-student-from v-on:student-added="newStudentAdded"></new-student-from>
+  <student-table v-bind:students="students" v-on:student-arrived-or-left="studentArrivedOrLeft"
+                  v-on:delete-student="studentDeleted"></student-table>
+  <student-message v-bind:student="mostRecentStudent"></student-message>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import NewStudentFrom from './components/NewStudentFrom.vue'
+import StudentMessage from './components/StudentMessage.vue'
+import StudentTable from './components/StudentTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewStudentFrom,
+    StudentMessage,
+    StudentTable
+  },
+  data(){
+    return{
+      students:[],
+      mostRecentStudent:{}
+    }
+  },
+  methods:{
+    newStudentAdded(student){
+      this.students.push(student)
+      this.students.sort(function (s1,s2){
+        return s1.name.toLowerCase() < s2.name.toLowerCase() ? -1 : 1
+      })
+    },
+    studentArrivedOrLeft(student,present){
+      let updateStudent = this.students.find(function (s){
+        if (s.name === student.name && s.starID === student.starID){
+          return true
+        }
+      })
+
+      if (updateStudent){
+        updateStudent.present= present
+        this.mostRecentStudent = updateStudent
+      }
+    },
+    studentDeleted(student){
+      this.students = this.students.filter(function (s){
+        if (s !== student){
+          return true
+        }
+      })
+
+      this.mostRecentStudent = {}
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css";
 </style>
---
+++ src/components/StudentMessage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+    <div v-if="student.name">
+
+      <div class="alert alert-info" v-if="student.present">
+        Welcome, {{student.name}}
+      </div>
+
+      <div class="alert alert-success" v-else>
+        Goodbye, {{student.name}}. see you later!
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'StudentMesage',
+  props:{
+    student: Object
+  }
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
